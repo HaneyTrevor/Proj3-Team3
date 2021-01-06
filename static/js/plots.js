58c4f92e383a267bd268b294c5744c927348dce3
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2021-01-05 21:53:26 -0800

    j

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,8 +1,7 @@
 //Part 1
 var trace1 = {
-  x: ["beer", "wine", "martini", "margarita",
-    "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-  y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+  x: ["Doctoral degree", "Professional degree"," Master's degree", "Bachelor's degree", "Associate's degree", "Some college, no degree","High school diploma", "Less than a high school diploma"]
+  y: "income",
   type: "bar"
 };
 
