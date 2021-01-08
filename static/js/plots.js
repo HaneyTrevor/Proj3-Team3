1d41db6399816689a25c1258c46ddde44ff0975e
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2021-01-07 19:19:41 -0800

    changes

--- static/js/plots.js
+++ static/js/plots.js
@@ -20,12 +20,20 @@ function init() {
     data = [{
         x: ["Doctoral degree", "Professional degree"," Master's degree", "Bachelor's degree", "Associate's degree", "Some college, no degree","High school diploma", "Less than a high school diploma"],
         y: [ 1883, 1861, 1497, 1248, 887, 833, 746, 5.4],
-        type: "bar"
+        type: "bar",
+        marker: {color: 'crimson'}
      }];
+
+    // Define the plot layout
+    layout = {
+        title: "Educational Attainment vs Weekly Pay ($)",
+        xaxis: { title: "Educational Attainment" },
+        yaxis: { title: "Weekly Pay ($)" }
+    };
   
     var CHART = d3.selectAll("#plot").node();
   
-    Plotly.newPlot(CHART, data);
+    Plotly.newPlot(CHART, data, layout);
   }
   
   // Call updatePlotly() when a change takes place to the DOM
@@ -43,16 +51,27 @@ function init() {
     // Initialize x and y arrays
     var x = [];
     var y = [];
+    var title = [];
   
     switch(dataset) {
-      case "Weekly Pay by Educational Attainment":
+      case "dataset1":
         x = ["Doctoral degree", "Professional degree"," Master's degree", "Bachelor's degree", "Associate's degree", "Some college, no degree","High school diploma", "Less than a high school diploma"];
         y = [ 1883, 1861, 1497, 1248, 887, 833, 746, 5.4];
+        update = {
+            title: "Educational Attainment vs Weekly Pay ($)",
+            xaxis: { title: "Educational Attainment" },
+            yaxis: { title: "Weekly Pay ($)" }
+        };
         break;
   
       case "dataset2":
         x = ["Doctoral degree", "Professional degree"," Master's degree", "Bachelor's degree", "Associate's degree", "Some college, no degree","High school diploma", "Less than a high school diploma"];
         y = [1.1, 1.6, 2, 2.2, 2.7, 3.3, 3.7, 5.4];
+        update = {
+            title: "Educational Attainment vs Unemployment (%)",
+            xaxis: { title: "Educational Attainment" },
+            yaxis: { title: "Unemployment (%)" }
+        };
         break;
   
     //   case "dataset3":
@@ -63,6 +82,11 @@ function init() {
       default:
         x = ["Doctoral degree", "Professional degree"," Master's degree", "Bachelor's degree", "Associate's degree", "Some college, no degree","High school diploma", "Less than a high school diploma"];
         y = [ 1883, 1861, 1497, 1248, 887, 833, 746, 5.4];
+        update = {
+            title: "Educational Attainment vs Weekly Pay ($)",
+            xaxis: { title: "Educational Attainment" },
+            yaxis: { title: "Weekly Pay ($)" }
+        };
         break;
     }
   
@@ -70,7 +94,9 @@ function init() {
     // Note the extra brackets around 'x' and 'y'
     Plotly.restyle(CHART, "x", [x]);
     Plotly.restyle(CHART, "y", [y]);
+    Plotly.relayout(CHART, update);
   }
   
   init();
   
+
