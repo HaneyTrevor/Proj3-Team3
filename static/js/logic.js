3aaf2f113c945890f3849c20d4dc5180cf87162c
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2021-01-08 18:59:58 -0800

    vue js library

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,3 @@
-// var mapboxAccessToken = API_KEY;
 var map = L.map('map').setView([37.8, -96], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -71,4 +70,13 @@ d3.json(link, function(data) {
   // Adding legend to the map
   legend.addTo(map);
 
+});
+
+
+//////////////////////////////
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Vue Text'
+  }
 });
